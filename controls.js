e99a41fa37f4724e02cd7d08e27c0757a8b81833
alejandrorp <jerodriguez@powerstreet.com.mx>
2023-12-19 22:44:47 -0600

    adding basic car

---
+++ controls.js
@@ -0,0 +1,46 @@
+class Controls {
+  constructor() {
+    this.foward = false
+    this.left = false
+    this.right = false
+    this.reverse = false
+
+    this.#addKeyboardListeners()
+  }
+
+  #addKeyboardListeners() {
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case 'ArrowRight':
+          this.right = false
+          break
+        case 'ArrowUp':
+          this.foward = false
+          break
+        case 'ArrowDown':
+          this.reverse = false
+          break
+        case 'ArrowLeft':
+          this.left = false
+          break
+      }
+    }
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case 'ArrowRight':
+          this.right = true
+          break
+        case 'ArrowUp':
+          this.foward = true
+          break
+        case 'ArrowDown':
+          this.reverse = true
+          break
+        case 'ArrowLeft':
+          this.left = true
+          break
+      }
+    }
+
+  }
+}
